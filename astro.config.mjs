5fed94557061d3b19afeac5fe465c8ad8f01d0a1
Danny Tong <tzd2005@gmail.com>
2025-12-26 23:40:47 -0500

    fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.datscoresources.com',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      filename: 'sitemap.xml'
+    })
+  ],
 });
