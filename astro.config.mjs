4772b8b39bfe059debe2fe367eafa944881ced2d
Danny Tong <tzd2005@gmail.com>
2025-12-26 23:22:52 -0500

    add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.datscoresources.com',
+  integrations: [sitemap()],
+});
